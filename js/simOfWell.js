39e9332cdb91f00cd5f8bcd0fdbb755a5a5b95fa
Adrian Setniewski <274433@pw.edu.pl>
2018-12-09 19:58:25 +0100

    Particle in the box update.

--- js/simOfWell.js
+++ js/simOfWell.js
@@ -1,24 +1,57 @@
 class Simulation {
     constructor() {
-        this.N = 1000;
+        this.N = 100;
         this.dt = 0.0001;
-        this.kappa = 1.0;
-        this.n = 1;
-        this.omega = 5.0 / 0.5 * Math.PI * Math.PI;
-	this.dx = 1 / 100;
- 	this.width = document.getElementById("canvas").clientWidth;
-	this.ratio = this.N / this.width;
-	this.wellPositions = [ 450, 550];
-	this.wellHeights = [0];
- 	this.re = [];
+        this.kappa = 10.0;
+		this.n = 3;
+		this.omega = 5.0 / 2.0 * Math.PI * Math.PI;
+		this.dx = 1 / 100;
+		this.width = document.getElementById("canvas").clientWidth;
+		this.ratio = this.N / this.width;
+		this.wellHeights = [0];
+		this.re = [];
         this.im = [];
         this.hr = [];
         this.hi = [];
         this.p = [];
         this.x = [];
-	this.V = [];
-	this.tau = 0;
-	this. time = 0;
+		this.V = [];
+		this.tau = 0;
+		this.speed = 10;
+		this.time = 0;
+    }
+	
+	calculateNorm()
+	{
+		var ret = 1.;
+		ret *= this.dx;
+		for (var ii = 0; ii < 
+		
+		this.N + 1; ii++)
+			ret += (this.re[ii] * this.re[ii] + this.im[ii] * this.im[ii]);
+		return ret;
+	}
+	
+	calculateAvPos()
+	{ // calculate average position
+		var ret = 1.;
+		for (var ii = 0; ii < this.N + 1; ii++)
+			ret += (ii * this.dx * (this.re[ii] * this.re[ii] + this.im[ii] * this.im[ii]));
+		return ret*this.dx;
+	}
+	
+	
+	calculateAvEnergy()
+	{ // calculate average energy
+		var ret = 1.;
+		ret *= this.dx;
+		for (var ii = 0; ii < this.N + 1; ii++)
+			ret += (this.re[ii] * this.hr[ii] + this.im[ii] * this.hi[ii]);
+		return ret * this.dx;
+	}
+		
+	calculateHamiltonian(part, k) {
+        return -0.5 * (part[k + 1] + part[k - 1] - 2.0 * part[k]) / (this.dx * this.dx) + this.kappa * (k * this.dx - 0.5) * part[k] * Math.sin(this.omega * this.tau) ;
     }
 	
     calculateStationary(n, k) {
@@ -26,51 +59,58 @@ class Simulation {
     }
 
     update(){
-	for(var jj = 0; jj< this.wellPositions.length; jj+=2){
-      		for (var ii = this.wellPositions[jj]; ii < this.wellPositions[jj+1]; ii++)
-		{
-            		this.re[ii] = this.calculateStationary(this.n, ii-this.wellPositions[jj])*(Math.cos(this.time));
-            		this.x[ii] = ii;
-            		this.im[ii] = this.calculateStationary(this.n, ii-this.wellPositions[jj])*(Math.sin(this.time));
-	    		this.p[ii] = this.re[ii] * this.re[ii] + this.im[ii] * this.im[ii];
-			this.V[ii] = this.wellHeights[jj/2];
-        	}
-	}
+	for(var i =0; i<this.speed;i++)
+		this.leapFrog();
 	this.time+=0.01;
 
     }
 	
+	 leapFrog(){
+		for (var ii = 0; ii < this.N + 1; ii++)
+			this.re[ii] = this.re[ii] + this.hi[ii] * this.dt / 2.;
+		for (var jj = 1; jj < this.N; jj++)
+			this.hr[jj] = this.calculateHamiltonian(this.re, jj, this.tau);
+		for (var ii = 0; ii < this.N + 1; ii++)
+			this.im[ii] = this.im[ii] - this.hr[ii] * this.dt;
+		for (var jj = 1; jj < this.N; jj++)
+			this.hi[jj] = this.calculateHamiltonian(this.im, jj, this.tau);
+		for (var ii = 0; ii < this.N + 1; ii++)
+			this.re[ii] = this.re[ii] + this.hi[ii] * this.dt / 2.;
+		for (var ii = 0; ii < this.N + 1; ii++)
+		{
+			this.p[ii] = this.re[ii] * this.re[ii] + this.im[ii] * this.im[ii];
+		}
+		this.tau += this.dt;
+		
+    }
+
+	
     setup() {
         
-       
-	for(var jj = 0; jj< this.wellPositions.length; jj+=2){
-      		for (var ii = this.wellPositions[jj]; ii < this.wellPositions[jj+1]; ii++)
-		{
-            		this.re[ii] = this.calculateStationary(this.n, ii-this.wellPositions[jj]);
-            		this.x[ii] = ii;
-            		this.im[ii] = this.calculateStationary(this.n, ii-this.wellPositions[jj])*(Math.sin(this.time));
-	    		this.p[ii] = this.re[ii] * this.re[ii] + this.im[ii] * this.im[ii];
-			this.V[ii] = this.wellHeights[jj/2];
-        	}
-	}
+       this.tau = 0;
 
-	var arrTmp = [0];
-	var arr = arrTmp.concat(this.wellPositions);
-	arr.push(this.N);
-	//console.log(arr);
-	for(var jj = 0; jj< arr.length; jj+=2){
-      		for (var ii = arr[jj]; ii < arr[jj+1]; ii++)
+      		for (var ii = 0; ii < this.N+1; ii++)
 		{
-            		this.re[ii] = 0;
+            		this.re[ii] = this.calculateStationary(this.n, ii);
             		this.x[ii] = ii;
             		this.im[ii] = 0;
-	    		this.p[ii] = 0;
-			this.V[ii] = 1;
-
+					this.p[ii] = this.re[ii] * this.re[ii] + this.im[ii] * this.im[ii];
+					this.V[ii] = 10;
         	}
+	
+	
+	for (var ii = 0; ii < this.N+1; ii++)
+	{
+		if(ii == 0 || ii == this.N){
+			this.hr[ii] = 0;
+			this.hi[ii] = 0;
+		}else{
+			this.hr[ii] = this.calculateHamiltonian(this.re, ii, this.tau);
+			this.hi[ii] = this.calculateHamiltonian(this.im, ii, this.tau);
+		}
+		
 	}
-
-
+	
 	
     }
 	
@@ -79,7 +119,7 @@ class Simulation {
 class RenderIt{
 	constructor() {
 	this.t = 0;
-        this.N = 1000;
+        this.N = 101;
         this.width = document.getElementById("canvas").clientWidth;
         this.height = 700;
         this.positions = new Float32Array(this.N * 3);
@@ -96,6 +136,7 @@ class RenderIt{
 	this.lineIm = null;
 	this.sim = new Simulation();
  	this.ratio = this.N / this.width;
+	this.iter = 0;
 
     }
 	
@@ -117,23 +158,23 @@ class RenderIt{
 
 		geometry.addAttribute('position', new THREE.BufferAttribute(this.positions, 3));
 		geometry.setDrawRange(0, this.N);
-		var material = new THREE.LineBasicMaterial({ color: 0x00000, linewidth: 2 });
+		var material = new THREE.LineBasicMaterial({ color: 0x00ff00, linewidth: 2 });
 
 		this.line = new THREE.Line( geometry, material );
 
 		var geometry2 = new THREE.BufferGeometry();
-		var material2 = new THREE.LineBasicMaterial({ color: 0xff000, linewidth: 2 });
+		var material2 = new THREE.LineBasicMaterial({ color: 0x000000, linewidth: 2 });
 		geometry2.addAttribute('position', new THREE.BufferAttribute(this.positionsRe, 3));
 		geometry2.setDrawRange(0, this.N);
 		this.lineRe = new THREE.Line( geometry2, material2 );
 
 		var geometry3 = new THREE.BufferGeometry();
-		var material3 = new THREE.LineBasicMaterial({ color: 0xaf0f2, linewidth: 2 });
+		var material3 = new THREE.LineBasicMaterial({ color: 0x000000, linewidth: 2 });
 		geometry3.addAttribute('position', new THREE.BufferAttribute(this.positionsWell, 3));
 		geometry3.setDrawRange(0, this.N);
 		this.lineWell = new THREE.Line( geometry3, material3 );
 		var geometry4 = new THREE.BufferGeometry();
-		var material4 = new THREE.LineBasicMaterial({ color: 0xf6a52, linewidth: 2 });
+		var material4 = new THREE.LineBasicMaterial({ color: 0xff0000, linewidth: 2 });
 		geometry4.addAttribute('position', new THREE.BufferAttribute(this.positionsIm, 3));
 		geometry4.setDrawRange(0, this.N);
 		this.lineIm = new THREE.Line( geometry4, material4 );
@@ -149,7 +190,12 @@ class RenderIt{
 	}
 	
 	updatePositions(){
-				
+		if(this.iter%20 == 0){
+			addData(myChart, "", {x: this.iter/10, y:50*this.sim.calculateAvEnergy()});
+			addData(myChart2, "", {x: this.iter/10, y:50*this.sim.calculateAvPos()});
+			addData(myChart3, "", {x: this.iter/10, y:50*this.sim.calculateNorm()});
+			//myChartaddData();
+		}
 		this.sim.update();
 		for(var i=0;i<3 *( this.N+1); i+=3){
 			this.positions[ i ] = (-0.9*this.width/2+i*0.9/(this.ratio*3))
@@ -171,11 +217,148 @@ class RenderIt{
 
 
 		}
+		
+		this.iter++;
 				
 	}
 	
 };
+var ctx = document.getElementById("myChart").getContext('2d');
+var ctx2 = document.getElementById("myChart2").getContext('2d');
+var ctx3 = document.getElementById("myChart3").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'scatter',
+	fontColor : "#ffa45e",
+   data: {
+      datasets: [{
+         label: "Average energy",
+		 data: [],
+		 fillColor: "rgba(0, 138, 212,0.5)",
+		 pointBackgroundColor : "rgba(0, 138, 212,0.5)"
+      }]
+   },  
+   options: {
+        scaleFontColor: 'red',
+        responsive: true,
+        tooltips: {
+            mode: 'single',
+        },
+        scales: {
+            xAxes: [{ 
+                gridLines: {
+                    display: true,
+					color: "#000000"
+                },
+                ticks: {
+                  fontColor: "#000000",
+                },
+            }],
+            yAxes: [{
+                display: true,
+                gridLines: {
+                    display: true,
+					color: "#000000"
+                },
+                ticks: {
+                  fontColor: "#000000",
+                },
+            }],
+        }
+    }    
+});
 
+var myChart2 = new Chart(ctx2, {
+    type: 'scatter',
+	fontColor : "#ffa45e",
+   data: {
+      datasets: [{
+         label: "Average position",
+		 data: [],
+		 fillColor: "rgba(0, 138, 212,0.5)",
+		 pointBackgroundColor : "rgba(0, 138, 212,0.5)"
+      }]
+   },  
+   options: {
+        scaleFontColor: 'red',
+        responsive: true,
+        tooltips: {
+            mode: 'single',
+        },
+        scales: {
+            xAxes: [{ 
+                gridLines: {
+                    display: true,
+					color: "#000000"
+                },
+                ticks: {
+                  fontColor: "#000000",
+                },
+            }],
+            yAxes: [{
+                display: true,
+                gridLines: {
+                    display: true,
+					color: "#000000"
+                },
+                ticks: {
+                  fontColor: "#000000",
+                },
+            }],
+        }
+    }    
+});
+
+var myChart3 = new Chart(ctx3, {
+    type: 'scatter',
+	fontColor : "#ffa45e",
+   data: {
+      datasets: [{
+         label: "Norm",
+		 data: [],
+		 fillColor: "rgba(0, 138, 212,0.5)",
+		 pointBackgroundColor : "rgba(0, 138, 212,0.5)"
+      }]
+   },  
+   options: {
+        scaleFontColor: 'red',
+        responsive: true,
+        tooltips: {
+            mode: 'single',
+        },
+        scales: {
+            xAxes: [{ 
+                gridLines: {
+                    display: true,
+					color: "#000000"
+                },
+                ticks: {
+                  fontColor: "#000000",
+                },
+            }],
+            yAxes: [{
+                display: true,
+                gridLines: {
+                    display: true,
+					color: "#000000"
+                },
+                ticks: {
+                  fontColor: "#000000",
+                },
+            }],
+        }
+    }    
+});
+
+
+function addData(chart, label, data) {
+    chart.data.datasets[0].data.push({x: data.x, y: data.y});
+    chart.update();
+}
+
+function clearData(chart) {
+    chart.data.datasets[0].data = [];
+    chart.update();
+}
 var abc = new RenderIt();
 abc.init();
 
@@ -190,11 +373,55 @@ this.animate = function() {
 			abc.renderer.render(abc.scene, abc.camera);
 		           
 }
-var reset = document.getElementById("reset");
-reset .addEventListener("input", function(e) {
+var resetn = document.getElementById("reset_n");
+resetn.addEventListener("input", function(e) {
 		            var target = (e.target) ? e.target : e.srcElement;
 		            abc.sim.n= (target.value);
 		            abc.sim.setup();
+					clearData(myChart);
+					clearData(myChart2);
+					clearData(myChart3);
+					abc.iter = 0;
+					var div = document.getElementById("sim_n_val");
+					div.innerHTML = "$$n={" + e.target.value + "}$$";
+					MathJax.Hub.Queue(["Typeset",MathJax.Hub,"sim_n_val"]);
+					
+
+});
+var resetw = document.getElementById("reset_w");
+resetw.addEventListener("input", function(e) {
+		            var target = (e.target) ? e.target : e.srcElement;
+		            abc.sim.omega= (target.value)/ 2.0 * Math.PI * Math.PI;
+		            abc.sim.setup();
+					clearData(myChart);
+					clearData(myChart2);
+					clearData(myChart3);
+					abc.iter = 0;
+					var div = document.getElementById("sim_w_val");
+					div.innerHTML = "$$\\frac{" + e.target.value + "}{2}\\cdot \\pi^2$$";
+					MathJax.Hub.Queue(["Typeset",MathJax.Hub,"sim_w_val"]);
+
+});
+var resetk = document.getElementById("reset_k");
+resetk.addEventListener("input", function(e) {
+		            var target = (e.target) ? e.target : e.srcElement;
+		            abc.sim.kappa= (target.value);
+		            abc.sim.setup();
+					clearData(myChart);
+					clearData(myChart2);
+					clearData(myChart3);
+					abc.iter = 0;
+					var div = document.getElementById("sim_k_val");
+					div.innerHTML = "$$\\kappa="+e.target.value+"$$";
+					MathJax.Hub.Queue(["Typeset",MathJax.Hub,"sim_k_val"]);
+
+});
+
+var speed = document.getElementById("reset_speed");
+speed.addEventListener("input", function(e) {
+		            var target = (e.target) ? e.target : e.srcElement;
+		            abc.sim.speed= (target.value);
+
 
 });
 if (WEBGL.isWebGLAvailable()) {
@@ -204,6 +431,8 @@ if (WEBGL.isWebGLAvailable()) {
 	var warning = WEBGL.getWebGLErrorMessage();
 	document.getElementById('canvas').appendChild(warning);
 }
+
+
 
 //Render.init();
 //	Render.destroy();
